63a0327a14677880be40d68111f9a316e8592981
Vuong Nguyen <vnguyen@theagencyonline.co.uk>
2019-11-16 00:19:18 +0000

    add files

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var pkg = require('./package.json');
+
+
+var autoprefixerOptions = {
+    overrideBrowserslist: [
+      "last 2 version",
+      "> 1%",
+      "maintained node versions",
+      "not dead"
+    ],
+    cascade: false
+};
+
+var path = {
+    css_dir : 'assets/css',
+    sass_files : 'assets/sass/**/*.scss',
+    js_dir: 'assets/js'
+}
+
+gulp.task('build-sass', function () {
+    return gulp.src(path.sass_files)
+      .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(autoprefixer(autoprefixerOptions))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest(path.css_dir));
+});
+
+gulp.task('deploy-sass', function () {
+    return gulp.src(path.sass_files)
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(autoprefixer(autoprefixerOptions))
+      .pipe(gulp.dest(path.css_dir));
+});
+
+// Minify JS
+gulp.task('minify-js', function (cb) {
+    pump([
+        gulp.src(path.js_dir),
+        concat(path.js_dir + '/main.js'),
+        rename('main.min.js'),
+        uglify(),
+        gulp.dest(path.js_dir)
+    ],
+    cb
+    );
+});
+
+gulp.task('build', gulp.series('deploy-sass', 'minify-js'));
+
+gulp.task('watch', gulp.parallel(function() {
+    gulp.watch(path.js_dir, gulp.series(['minify-js']));
+    gulp.watch(path.sass_files, gulp.series(['build-sass']));
+}));
+
+gulp.task('dev', gulp.series('build', 'watch'));
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Shaping brands. Connecting audiences. By thinking louder. Seeing bigger. Being bolder. Killing the silence.">
+        <title>Holding page</title>
+        <link rel="stylesheet" href="/assets/css/style.css" />
+    </head>
+    <body>
+        <main class="site__main">
+            <header class="site__section header">
+                <div class="site__topbar">
+                    Call us on <a class="cta" href="">0117 927 7841</a> or drop us a message <a class="cta" href="mailto:hello@wearehalo.co.uk">hello@wearehalo.co.uk</a>
+                </div>
+                <h1 class="header__title">Coming soon</h1>
+                <div class="progress">
+                    <span class="progress__start progress__figure">0%</span>
+                    <span class="progress__bar"></span>
+                    <span class="progress__end progress__figure">100%</span>
+                </div>
+                <h2 class="header__subtitle">This website is currently under construction.<br>Please come back soon.</h2>
+            </header>
+            <section class="site__section fadeIn">
+                <div class="form__container">
+                    <h3 class="form__title ">
+                        But before you go, why not subscribe to our mailing list?
+                    </h3>
+                    <form class="form" action="https://wearehalo.us20.list-manage.com/subscribe/post?u=ef1adad7b761018bee671bf45&amp;id=3b2a16ce2b"  method="post" target="_blank">
+                        <label hidden>Email</label>
+                        <input type="email" name="EMAIL" placeholder="Your email (e.g. email@example.com)" >
+                        <button type="submit">Send</button>
+                        <div style="position: absolute; left: -5000px;" aria-hidden="true" hidden>
+                            <input type="text" name="b_ef1adad7b761018bee671bf45_3b2a16ce2b" tabindex="-1" value="">
+                        </div>
+                    </form>
+                </div>
+            </section>
+
+            <footer class="site__footer">
+                <nav class="site__nav--footer">
+                    <a class="nav__item cta" href="https://wearehalo.co.uk/cookies-and-privacy/">Cookies & Privacy</a>
+                </nav>
+            </footer>
+        </main>
+    </body>
+</html>
