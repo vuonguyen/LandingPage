80c9445b8c25843c89de4d42d1d7db98907eb49a
Vuong Nguyen <vnguyen@theagencyonline.co.uk>
2019-11-17 23:17:48 +0000

    add logo to the page, add footer background, move share icons to footer

--- index.php
+++ index.php
@@ -15,18 +15,21 @@
                 <div class="site__topbar">
                     Call us on <a class="cta" href="">0117 927 7841</a> or drop us a message <a class="cta" href="mailto:hello@wearehalo.co.uk">hello@wearehalo.co.uk</a>
                 </div>
+
+                <svg class="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 59 59"><g fill="#fff" fill-rule="evenodd"><path d="M0 58.915h12.274V0H0zM46.641 58.915h12.274V0H46.641zM35.008 18.415c-2.34 0-4.4 1.241-5.604 3.123-1.204-1.882-3.263-3.123-5.603-3.123-3.714 0-6.724 3.123-6.724 6.975 0 8.136 8.965 12.204 12.327 15.11 3.362-2.906 12.328-6.974 12.328-15.11 0-3.852-3.011-6.975-6.724-6.975"/></g></svg>
+
                 <h1 class="header__title">Coming soon</h1>
                 <div class="progress">
                     <span class="progress__start progress__figure">0%</span>
                     <span class="progress__bar"></span>
                     <span class="progress__end progress__figure">100%</span>
                 </div>
-                <h2 class="header__subtitle">This website is currently under construction.<br>Please come back soon.</h2>
+                <h2 class="header__subtitle">This website is currently under construction.</h2>
             </header>
             <section class="site__section fadeIn">
                 <div class="form__container">
                     <h3 class="form__title ">
-                        But before you go, why not subscribe to our mailing list?
+                        Before you go, why not subscribe to our mailing list?
                     </h3>
                     <form class="form" action="https://wearehalo.us20.list-manage.com/subscribe/post?u=ef1adad7b761018bee671bf45&amp;id=3b2a16ce2b"  method="post" target="_blank">
                         <label hidden>Email</label>
@@ -38,14 +41,13 @@
                     </form>
                 </div>
             </section>
-            <?php include 'parts/social.php' ?>
+            <?php //include 'parts/social.php' ?>
 
             <footer class="footer">
                 <nav class="site__nav--footer">
                     <a class="nav__item cta" href="https://wearehalo.co.uk/cookies-and-privacy/">Cookies & Privacy</a>
-
-
                 </nav>
+                <?php include 'parts/social.php' ?>
             </footer>
         </main>
     </body>
